39ceb58425f9f04471c99479d914b886b237e70b
MuratMucur <d00273234@student.dkit.ie>
2024-11-23 23:48:40 +0000

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,29 @@
+﻿namespace CarRental
+{
+    public abstract class Car
+    {
+        //fields
+        private string description = "Car Rental";
+
+        //properties
+        public string Description { 
+        get { return description; }
+        set { description = value; }
+
+        }
+        public abstract double GetCost();
+
+    }
+    public abstract class CarDecorator : Car
+    {
+        public abstract string GetDescription();
+
+    }
+    public enum CarType
+    {
+        CityCar,
+        MiddleSize,
+        Luxury
+    }
+
+}
